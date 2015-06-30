f93b4ff6c59c36d4ed8a679d61319b5a6856f1ae
lpgoncalves <lp.gooncalves@gmail.com>
2015-06-30 00:13:14 -0300

    Criação Index - Branch Index

---
+++ PIC_MVC/Areas/Home/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIC_MVC.Areas.Home.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
